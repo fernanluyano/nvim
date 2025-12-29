22f85d2a8caa3a6d771e5d4cc95c431be22014b2
Fernando <fernando.berlanga1@gmail.com>
2025-12-28 21:06:44 -0500

    added compilation cmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>Cc", "<cmd>terminal make compile<CR>", { desc = "Compile Project" })
